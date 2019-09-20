f2803324d49ecab2456af21987e0a0011d60e964
haidooora <haidooora1@gmail.com>
2019-09-20 19:33:37 +0300

    hhh

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-=======
-// import logo from './logo.svg';
->>>>>>> 36fc8d2de267567d1f0a2d9c4f9053558bd72724
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
       <p>harakat</p>
-=======
-  <p>harakat</p>
->>>>>>> 36fc8d2de267567d1f0a2d9c4f9053558bd72724
       </div>
     );
   }
