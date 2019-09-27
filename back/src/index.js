b9824c25f0c4e4646b6a3a6a614b10ba1409b7c6
hovigkevorkian <hovikkevork@gmail.com>
2019-09-27 13:27:57 +0300

    whatever

--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,5 @@
 import app from "./app";
 import gallery_videos from "./db/gallery_videos.js";
-<<<<<<< HEAD
 import gallery_images from "./db/gallery_images";
 // import { isloggedIn, authenticateUser, logout } from "./auth.js";
 import path from 'path'
@@ -392,69 +391,3 @@ const start = async () => {
 app.listen( 8080, () => console.log('server listening on port 8080') )
 
 module.exports=app;
-=======
-import { isloggedIn, authenticateUser, logout } from "./auth.js";
-import initializeDatabase from "./db/about_us";
-import path from 'path'
-
-
-
-
-const start = async () => {
-  const controller = await initializeDatabase();
-
-  /* const id = await controller.createContact({name:"Brad Putt",email:"brad@pet.com"})
-  const contact = await controller.getContact(id)
-  console.log("------\nmy newly created contact\n",contact)
- // await controller.updateContact(id, {name:"Brad Pitt"})
-  await controller.updateContact(id, {email:"brad@pitt.com"})
-  const updated_contact = await controller.getContact(id)
-  console.log("------\nmy updated contact\n",updated_contact)
-  console.log("------\nlist of contacts before\n",await controller.getContactsList())
-  await controller.deleteContact(id)
-  console.log("------\nlist of contacts after deleting\n",await controller.getContactsList())
- */
-
-  /**
-   * Route that returns string ok
-   * @module /
-   * @param {Express.Request} req - request
-   * @param {Express.Response} res - response
-   * @return {string}  - ok
-   */
-  app.get("/", async (req, res) => {
-    const about = await controller.getAbout()
-    res.json({
-      data:about
-    })
-  });
-
-  /**
-   * Route that returns a list of contacts
-   * @module contacts/list
-   * @param {Express.Request} req - request
-   * @param {Express.Response} res - response 
-   * @return {json} rows - array of contacts
-   */
-  // app.get("/gallery", async (req, res, next) => {
-  //   try {
-  //     const { link_url, id } = req.query;
-  //     const rows = await controller.getgallery_vidoes
-  //     ({ link_url, id });
-
-  //     res.json({ success: true, result: rows });
-  //   } catch (err) {
-  //     next(err);
-  //   }
-  // });
-
- 
-
-  app.listen(8080, () => {
-    console.log("server listening on port 8080");
-  });
-};
-start();
-//export default app;
-module.exports=app;
->>>>>>> 9b83f2b97293739816e747827b0efba334462ad6
